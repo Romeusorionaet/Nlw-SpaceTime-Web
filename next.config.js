c844924e23caf83997698d1a1d0437e9a06b4db2
romeusorionaet <romeusoares14569@gmail.com>
2023-06-23 11:21:28 -0300

    preview

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['avatars.githubusercontent.com', '192.168.18.41'],
+  },
+}
 
 module.exports = nextConfig
---
+++ src/components/MediaPicker.tsx
@@ -0,0 +1,38 @@
+'use client'
+import { useState, ChangeEvent } from 'react'
+
+export function MediaPicker() {
+  const [preview, setPreview] = useState<string | null>(null)
+
+  function onFileSelected(event: ChangeEvent<HTMLInputElement>) {
+    const { files } = event.target
+
+    if (!files) {
+      return
+    }
+    const previewURL = URL.createObjectURL(files[0])
+
+    setPreview(previewURL)
+  }
+
+  return (
+    <>
+      <input
+        onChange={onFileSelected}
+        name="coverUrl"
+        type="file"
+        id="media"
+        className="invisible h-0 w-0"
+      />
+
+      {preview && (
+        // eslint-disable-next-line
+        <img
+          src={preview}
+          alt=""
+          className="roudend-lg aspect-video w-full object-cover"
+        />
+      )}
+    </>
+  )
+}
