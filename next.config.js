9e4656aeb10d3823a77e911c61b005e6858cfbab
romeusorionaet <romeusoares14569@gmail.com>
2023-07-04 13:46:47 -0300

    new config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['avatars.githubusercontent.com', '192.168.18.41'],
+    // domains: ['avatars.githubusercontent.com', '192.168.18.41'],
+    domains: [
+      'avatars.githubusercontent.com',
+      '192.168.18.41',
+      'nlw-spacetime-3nhe.onrender.com',
+    ],
   },
 }
 
