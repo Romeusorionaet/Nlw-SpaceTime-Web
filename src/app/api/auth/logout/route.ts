4946e289560122b6159e4794687911902aa23456
romeusorionaet <romeusoares14569@gmail.com>
2023-07-10 16:11:34 -0300

    fix callback

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,7 +1,12 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(request: NextRequest) {
-  const redirectURL = new URL('/', request.url)
+  const redirectURL = new URL(
+    '/',
+    process.env.NEXT_PUBLIC_NEXTAUTH_URL
+      ? process.env.NEXT_PUBLIC_NEXTAUTH_URL
+      : request.url,
+  )
 
   return NextResponse.redirect(redirectURL, {
     headers: {
