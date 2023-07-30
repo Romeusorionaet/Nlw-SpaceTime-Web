c54059673bb1dc0166e1cb09cf6f295f949f92ca
romeusorionaet <romeusoares14569@gmail.com>
2023-07-30 10:46:47 -0300

    return config

--- next.config.js
+++ next.config.js
@@ -1,24 +1,11 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: [
       'avatars.githubusercontent.com',
       'localhost',
       'res.cloudinary.com',
-      'nlw-spacetime-3nhe.onrender.com',
     ],
-  },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-        ],
-      },
-    ]
   },
 }
 
